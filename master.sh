7f61fafe462a3c0c5548cc0c283b658847d097cc
Ayush Ganatra <65668878+Ayush122001@users.noreply.github.com>
2021-09-13 07:50:57 +0530

    Create master.sh

---
+++ master.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+export k8s_master=$(cat /master | head -n 1)
+ip=$k8s_master
+echo $k8s_master
+cat <<EOF | java -jar /root/jenkins-cli.jar -s  http://192.168.43.73:8080/  create-node k8s_master
+<slave>
+  <name>k8s_master</name>
+  <description></description>
+  <remoteFS>/j</remoteFS>
+  <numExecutors>1</numExecutors>
+  <mode>NORMAL</mode>
+  <retentionStrategy class="hudson.slaves.RetentionStrategy$Always"/>
+  <launcher class="hudson.plugins.sshslaves.SSHLauncher" plugin="ssh-slaves@1.31.6">
+    <host>${ip}</host>
+    <port>22</port>
+    <credentialsId>d7a48a3e-f35b-47d5-ba87-f83c672a5163</credentialsId>
+    <launchTimeoutSeconds>60</launchTimeoutSeconds>
+    <maxNumRetries>10</maxNumRetries>
+    <retryWaitTime>15</retryWaitTime>
+    <sshHostKeyVerificationStrategy class="hudson.plugins.sshslaves.verifiers.NonVerifyingKeyVerificationStrategy"/>
+    <tcpNoDelay>true</tcpNoDelay>
+  </launcher>
+  <label>k8s_master</label>
+  <nodeProperties/>
+</slave>
+EOF
+
+java -jar /root/jenkins-cli.jar -s http://192.168.43.73:8080/ -webSocket connect-node k8s_master
